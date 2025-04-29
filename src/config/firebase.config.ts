5f546e73b32ddaf85bb54f78ee3d166129aa599b
xploreicon <iconfavour005@gmail.com>
2025-04-29 15:43:19 +0100

    tried fixing loggin in  commit

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
 const firebaseConfig = {
   apiKey: "AIzaSyC9F5kC3JAGMgngyNvZZ0hZZLKfnlhJTDs",
     authDomain: "afforgadgetable.firebaseapp.com",
@@ -12,4 +14,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 export { db };
